3fe2ec509fbdacb0c47ea7f190558ed1376dbd52
lucadisanto7 <lucadisanto47@gmail.com>
2024-06-27 15:07:13 +0200

    [UPD] Added Grid in html page

--- js/javascript.js
+++ js/javascript.js
@@ -2,19 +2,20 @@ const grid = document.getElementById('fizzbuzz-grid');
 for (let i = 1; i <= 100; i++) {
 
     const square = document.createElement('div')
+    square.classList.add('d-flex', 'justify-content-center', 'align-items-center', 'border', 'rounded', 'm-1', 'p-2')
 
     if (i % 3 === 0 && i % 5 === 0) {
         square.innerText = 'Fizzbuzz';
-        square.classList.add = 'fizzBuzz';
+        square.classList.add = ('bg-danger');
     } else if (i % 3 === 0) {
         square.innerText = 'Fizz';
-        square.classList.add = 'fizz';
+        square.classList.add = ('bg-success');
     } else if (i % 5 === 0) {
         square.innerText = 'Buzz';
-        square.classList.add = 'buzz';
+        square.classList.add = ('bg-warning');
     } else {
         square.textContent = i;
+        square.classList.add('bg-primary');
     }
-    console.log(grid)
-    console.log(square)
+    grid.appendChild(square);
 }
