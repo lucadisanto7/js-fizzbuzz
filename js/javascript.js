30691428371c75c930000e720604a1c338255d36
lucadisanto7 <lucadisanto47@gmail.com>
2024-06-27 15:09:09 +0200

    [FIX] Fixed js and added comments

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,20 @@
 const grid = document.getElementById('fizzbuzz-grid');
 for (let i = 1; i <= 100; i++) {
-
-    const square = document.createElement('div')
-    square.classList.add('d-flex', 'justify-content-center', 'align-items-center', 'border', 'rounded', 'm-1', 'p-2')
-
+    const square = document.createElement('div');
+    square.classList.add('d-flex', 'justify-content-center', 'align-items-center', 'border', 'rounded', 'm-1', 'p-2');
+    
     if (i % 3 === 0 && i % 5 === 0) {
-        square.innerText = 'Fizzbuzz';
-        square.classList.add = ('bg-danger');
+        square.innerText = 'FizzBuzz';
+        square.classList.add('bg-danger');  // Per rendere "FizzBuzz" di colore rosso
     } else if (i % 3 === 0) {
         square.innerText = 'Fizz';
-        square.classList.add = ('bg-success');
+        square.classList.add('bg-success');  // Per rendere "Fizz" di colore verde
     } else if (i % 5 === 0) {
         square.innerText = 'Buzz';
-        square.classList.add = ('bg-warning');
+        square.classList.add('bg-warning');  // Per rendere "Buzz" di colore giallo
     } else {
         square.textContent = i;
-        square.classList.add('bg-primary');
+        square.classList.add('bg-primary');  // Per rendere i numeri di colore blu
     }
     grid.appendChild(square);
 }
